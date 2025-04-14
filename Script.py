0ff99708c369c70d20d915847129023c1e88875a
tharabaapu <botterabaaphai@gmail.com>
2025-04-14 13:18:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -311,7 +311,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/SilentXBotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/SilentXBotz>[SilentXBotz]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/VIPSRM">{file_name}</a></b>\n\n<b>⚜️ By : <a https://t.me/+M3hQciIbuBc3YTZl>[SRM_Boss_Botz]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
